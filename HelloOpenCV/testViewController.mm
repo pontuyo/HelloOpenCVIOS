52f2d304264769655ad6beaf40c1687b0dbe3c8d
pontuyo <pontuyo@yagni.jp>
2013-02-18 00:06:43 +0900

    test

--- HelloOpenCV/testViewController.mm
+++ HelloOpenCV/testViewController.mm
@@ -21,6 +21,12 @@
     UIImage *image = [UIImage imageNamed:@"climbing.jpg"];
     UIImage *resizeImage = [self resizeImage:image rect:CGRectMake(0,0,320,480)];
     
+    cv::Mat srcMat = [self cvMatFromUIImage:resizeImage];
+    cv::Mat greyMat;
+    cv::cvtColor(srcMat, greyMat, CV_BGR2GRAY);
+    resizeImage = nil;
+    resizeImage = [self UIImageFromCVMat:greyMat];
+    
     UIImageView *imageView = [[UIImageView alloc]init];
     imageView.center = self.view.center;
     //imageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |UIViewAutoresizingFlexibleBottomMargin | [UIViewAutoresizingFlexibleTopMargin;
@@ -28,6 +34,7 @@
 
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     imageView.image = resizeImage;
+    imageView.alpha = 0.5f;
 
                                                                                                                                                          
     [self.view addSubview:imageView];
@@ -75,6 +82,41 @@
     return cvMat;
 }
 
-
+- (UIImage *)UIImageFromCVMat:(cv::Mat)cvMat
+{
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+    CGColorSpaceRef colorSpace;
+    
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(cvMat.cols,                                 //width
+                                        cvMat.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * cvMat.elemSize(),                       //bits per pixel
+                                        cvMat.step[0],                              //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
 
 @end
